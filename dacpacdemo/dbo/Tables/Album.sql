0f98526598cdfb286f8c8069b7da9ce540f29afa
sudhirsharma14 <sudhirsharma143@gmail.com>
2019-03-10 14:19:06 +0530

    as

--- dacpacdemo/dbo/Tables/Album.sql
+++ dacpacdemo/dbo/Tables/Album.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Album] (
     [AlbumId]  INT            NOT NULL,
-    [Title]    NVARCHAR (250) NOT NULL,
+    [Title]    NVARCHAR (160) NOT NULL,
     [ArtistId] INT            NOT NULL,
     CONSTRAINT [PK_Album] PRIMARY KEY CLUSTERED ([AlbumId] ASC),
     CONSTRAINT [FK_AlbumArtistId] FOREIGN KEY ([ArtistId]) REFERENCES [dbo].[Artist] ([ArtistId])
